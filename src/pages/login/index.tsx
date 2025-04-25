33bd15d54b5a6dccf51a025c4c3615b792be021a
Kaustubh Chaudhari <124234164+kaustubhchaudhari05@users.noreply.github.com>
2025-04-26 01:04:46 +0530

    userdetails

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -23,6 +23,7 @@ const LoginPage = () => {
         try {
             const res = await axios.post(`${endpoint}/login`, formData);
             console.log(res, "res");
+            localStorage.setItem('userDetails', JSON.stringify(res?.data?.data));
             if (res?.status === 200) {
                 navigate('/dashboard');
             }
@@ -72,54 +73,54 @@ const LoginPage = () => {
         // </>
 
         <>
-        <Container fluid className="register-container d-flex align-items-center justify-content-center">
-            <Row className="register-box shadow-lg rounded-4 overflow-hidden">
-                <Col md={6} className="left-panel d-none d-md-flex align-items-center justify-content-center">
-                    <img
-                        src="https://cdn-icons-png.flaticon.com/512/906/906334.png"
-                        alt="Illustration"
-                        className="img-fluid"
-                        style={{ maxHeight: '300px' }}
-                    />
-                </Col>
-                <Col md={6} className="p-5 right-panel">
-                    <div className="text-end mb-3">
-                        <small>Don't have account</small>{' '}
-                        <Button variant="link" className="p-0" onClick={() => navigate('/')}>Sign up</Button>
-                    </div>
-
-                    <h2 className="mb-2">Welcome to Focus!</h2>
-                    <p className="text-muted mb-4">Login to your account</p>
-
-                    <Form onSubmit={handleSubmit}>
-
-                        <Form.Group className="mb-3" controlId="formEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" name="email" placeholder="Enter your email" value={formData?.email} onChange={handleChange} />
-                        </Form.Group>
-
-                        <Form.Group className="mb-4" controlId="formPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" name="password" placeholder="8+ characters" value={formData?.password} onChange={handleChange} />
-                        </Form.Group>
-
-                        <Button variant="primary" className="w-100 mb-3" type="submit">
-                            Login
-                        </Button>
-                    </Form>
-
-                    <div className="text-center mt-3">
-                        <p className="text-muted">Create account with</p>
-                        <div className="d-flex justify-content-center gap-3">
-                            <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" width={28} />
-                            <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="LinkedIn" width={28} />
-                            <img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="Google" width={28} />
+            <Container fluid className="register-container d-flex align-items-center justify-content-center">
+                <Row className="register-box shadow-lg rounded-4 overflow-hidden">
+                    <Col md={6} className="left-panel d-none d-md-flex align-items-center justify-content-center">
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/906/906334.png"
+                            alt="Illustration"
+                            className="img-fluid"
+                            style={{ maxHeight: '300px' }}
+                        />
+                    </Col>
+                    <Col md={6} className="p-5 right-panel">
+                        <div className="text-end mb-3">
+                            <small>Don't have account</small>{' '}
+                            <Button variant="link" className="p-0" onClick={() => navigate('/')}>Sign up</Button>
                         </div>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-        {/* <div className="d-flex w-100">
+
+                        <h2 className="mb-2">Welcome to Focus!</h2>
+                        <p className="text-muted mb-4">Login to your account</p>
+
+                        <Form onSubmit={handleSubmit}>
+
+                            <Form.Group className="mb-3" controlId="formEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" name="email" placeholder="Enter your email" value={formData?.email} onChange={handleChange} />
+                            </Form.Group>
+
+                            <Form.Group className="mb-4" controlId="formPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" name="password" placeholder="8+ characters" value={formData?.password} onChange={handleChange} />
+                            </Form.Group>
+
+                            <Button variant="primary" className="w-100 mb-3" type="submit">
+                                Login
+                            </Button>
+                        </Form>
+
+                        <div className="text-center mt-3">
+                            <p className="text-muted">Create account with</p>
+                            <div className="d-flex justify-content-center gap-3">
+                                <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" width={28} />
+                                <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="LinkedIn" width={28} />
+                                <img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="Google" width={28} />
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            {/* <div className="d-flex w-100">
             <div className="d-flex w-100">
             <h4 className="align-items-start">Hello!!!</h4>
             <div className="align items-end">
@@ -128,7 +129,7 @@ const LoginPage = () => {
             </div>
             </div>
         </div> */}
-    </>
+        </>
     )
 }
 
